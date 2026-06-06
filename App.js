26dbefc9a1aad3d59f1469ecf85ffb1cc1871592
RPeckover <Henry.peckover19@bathspa.ac.uk>
2026-06-06 22:06:53 +0100

    Added old Genre Generator app's 'App.js' code into the current file.
    Added old Genre Generator app's 'App.js' code into the current file. Random functionality is broken due to lodash's import causing errors. File pathways also appear to cause issues.

--- App.js
+++ App.js
@@ -1,20 +1,650 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  Modal,
+  TouchableOpacity,
+} from "react-native";
+// import { random } from "lodash";
+// FIND OUT HOW TO IMPORT RANDOMISATION FUNCTIONALITY - LODASH CAUSING ERRORS CURRENTLY
+
+
+// Replace lylac with: 88D498 
+
+// Replace pink with: 1A936F
+
+// Replace turqoise with: 114B5F
 
 export default function App() {
+  const [newGenre, setNewGenre] = useState(""); //at some point take random words from arrays, use setNewGenre function to plug the array strings into a text field to output to user
+  const genreArray = [
+    "Electronic",
+    "House",
+    "Jazz",
+    "Rock",
+    "Disco",
+    "Pop",
+    "Metal",
+    "Classical",
+    "Rap",
+    "Rock",
+    "Hip-hop",
+  ];
+
+  const modifyerArray = [
+    "Psychadelic",
+    "Italo",
+    "Heavy",
+    "Pirate",
+    "Robot",
+    "Egyptian",
+    "Euro",
+    "Industrial",
+    "Halloween",
+    "Dance",
+    "Power",
+    "Space",
+    "Cowboy",
+    "Medieval",
+    "Deep",
+    "Progressive",
+  ];
+
+  const [modalVisible, setModalVisable] = useState(false);
+
+  const imageSelect = useState(null); //IS THIS WORKABLE AS A STATE VARIABLE FOR SECOND RANDOM GENRE FUNCTION
+
+  const [buttonsPressed, setButtonPressed] = useState(0);
+
+  const [tempGenre, setTempGenre] = useState("");
+
+  const randomGenre = () => {};
+
+  const emptyGenerate = () =>
+    modifyerArray[random(modifyerArray.length - 1)] +
+    " " +
+    genreArray[random(genreArray.length - 1)];
+
+  //setNewGenre genreArray
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <>
+        <TouchableOpacity
+          style={{
+            width: 80,
+            height: 40,
+            backgroundColor: "#62ECE1",
+            fontSize: 14,
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 20,
+            borderRadius: 10,
+            alignSelf: "flex-end",
+            marginTop: 25,
+            marginRight: 5,
+          }}
+          onPress={() => setModalVisable(true)}
+        >
+          <Text style={{ fontSize: 14 }}>Help</Text>
+        </TouchableOpacity>
+        <Modal visible={modalVisible} animationType={"slide"}>
+          <View style={styles.modal}>
+            <Text
+              style={{
+                fontSize: 16,
+                marginTop: 25,
+                marginBottom: 10,
+                textAlign: "center",
+                padding: 30,
+              }}
+            >
+              Generate a custom genre by clicking 'generate' or by selecting
+              images from the scroll bar!
+            </Text>
+            <TouchableOpacity
+              style={{
+                backgroundColor: "#62ECE1",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "20%",
+                height: 40,
+                borderRadius: 10,
+              }}
+              onPress={() => setModalVisable(false)}
+            >
+              <Text style={{ fontFamily: "sans-serif" }}>Done</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </>
+
+      {/* Put code for modal button to make modal visible (place in upper right corner) */}
+
+      <Text
+        style={{
+          marginTop: 10,
+          marginBottom: 25,
+          textAlign: "center",
+          fontSize: 30,
+        }}
+      >
+        {newGenre}
+      </Text>
+      <TouchableOpacity
+        style={{
+          width: 120,
+          height: 60,
+          backgroundColor: "#62ECE1",
+          fontSize: 20,
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: 20,
+          borderRadius: 10,
+        }}
+        onPress={() => {
+          if (tempGenre === "") {
+            setNewGenre(emptyGenerate());
+          } else setNewGenre(tempGenre);
+          setButtonPressed(0); //sets the buttonPressed state to 0
+          setTempGenre("");
+        }}
+        /*PRINT tempGenre IN THE GENRE GENERATION TEXT SPOT - making tempGenre eaqual to newGenre doesnt work*/
+      >
+        <Text style={{ fontSize: 18 }}>Generate!</Text>
+      </TouchableOpacity>
+
+      <ScrollView
+        style={{ width: "100%", height: "100%", backgroundColor: "#BAE9FF" }}
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            flexWrap: "wrap",
+            width: "80%",
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Classical");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/classical.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Country");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/country.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Cowboy");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/cowboy.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Dance");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/dance.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Disco");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/disco.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Egyptian");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/egyptian.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Electronic");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/electronic.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Euro");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/euro.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Halloween");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/halloween.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Heavy");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/heavy.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Hip-hop");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/hiphop.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "House");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/house.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Industrial");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/industrial.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Italo");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/italo.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Jazz");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/jazz.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Medieval");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/medieval.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Metal");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/metal.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Pirate");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/pirate.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Pop");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/pop.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Power");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/power.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Psychadelic");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/psychadelic.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Rap");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/rap.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Deep");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/deep.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Robot");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/robot.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Rock");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/rock.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Space");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/space.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              if (buttonsPressed > 4) {
+                alert("Too many images selected");
+              } else {
+                setTempGenre(tempGenre + " " + "Progressive");
+                setButtonPressed(buttonsPressed + 1);
+              }
+            }}
+          >
+            <Image
+              source={require("./assets/progressive.jpg")}
+              style={styles.image}
+            ></Image>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
+/*
+set up two horizontal scrollviews (one for each array) inside of each have touchableOpacity, 
+inside each of those, have an image - when setting image sizes, set width and height to 'undefined', set the resize mode to 'contain'
+*/
+
+//if using horizonal scroll, direct the user to scroll in this manner with UI (arrow plus the word 'scroll' or similar)
+
+//Math.floor(Math.random * INSERT ARRAY LENGTH)
+
+//use %s for slacling to keep consistant accross devices
+
+//Feedback - make sure user knows they have selected an image and can select more
+
+//could have two columns of images using flex wrap, make images a little larger
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FEDDFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 110,
+    height: 110,
+    aspectRatio: 1,
+    margin: 10,
+    resizeMode: "contain",
+  },
+  //check if it's possible to use % for image size rather than hard coding
+  modal: {
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+    backgroundColor: "#FEDDFF",
   },
 });
+
